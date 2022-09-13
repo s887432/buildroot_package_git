5c697cb30fe0196bdb752974dfe7e99054a19641
Patrick Lin <patrick.cclin@gmail.com>
2022-09-13 21:05:42 +0800

    Update helloworld.mk

--- helloworld.mk
+++ helloworld.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 HELLOWORLD_VERSION = 1.0
-HELLOWORLD_SITE = https://github.com/s887432/buildroot_package_src.git
+HELLOWORLD_SITE = https://github.com/s887432/buildroot_helloworld.git
 HELLOWORLD_SITE_METHOD = git
 HELLOWORLD_GIT_SUBMODULES = YES
 HELLOWORLD_LICENSE = Apache-2.0
