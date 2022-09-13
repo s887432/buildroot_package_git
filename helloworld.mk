844092a875eac9de5d1084dd1d69db31d71e39cc
Patrick Lin <patrick.cclin@gmail.com>
2022-09-13 21:22:32 +0800

    Update helloworld.mk

--- helloworld.mk
+++ helloworld.mk
@@ -11,6 +11,12 @@ HELLOWORLD_GIT_SUBMODULES = YES
 HELLOWORLD_LICENSE = Apache-2.0
 HELLOWORLD_INSTALL_TARGET = YES
 
-define HELLOWORLD_INSTALL_INIT
-  
+define HELLOWORLD_BUILD_CMDS
+    $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
 endef
+
+define HELLOWORLD_INSTALL_TARGET_CMDS
+        $(INSTALL) -D -m 0755 $(@D)/helloworld $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(generic-package))
